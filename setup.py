7396163b1672a5d8bef05bb3c99607ef9dc0f004
Stephen Newell <sjnewell@gmail.com>
2018-10-20 15:17:16 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ with open('README.rst') as f:
 
 setup(
     name="dev-pipeline-git",
-    version="0.2.0",
+    version="0.3.0",
     package_dir={
         "": "lib"
     },
     packages=find_packages("lib"),
 
     install_requires=[
-        'dev-pipeline-core >= 0.2.0',
-        'dev-pipeline-scm >= 0.2.0'
+        'dev-pipeline-core >= 0.3.0',
+        'dev-pipeline-scm >= 0.3.0'
     ],
 
     entry_points={
