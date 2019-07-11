ee950076aae819133da17f5de25af93bb05e1771
Stephen Newell <sjnewell@gmail.com>
2019-07-11 09:44:08 -0600

    Fix dependency issue with version 0.4.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 with open("README.rst") as f:
     long_description = f.read()
 
-_VERSION = "0.4.3"
+_VERSION = "0.4.0"
 
 setup(
     name="dev-pipeline-git",
